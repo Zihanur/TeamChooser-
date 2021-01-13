56424b6cc658e14cf5748cc5ca6fca993d45466f
Zihanur Rahman <71866520+Zihanur@users.noreply.github.com>
2021-01-13 12:10:25 +0600

    No Update

---
+++ TeamChooser.py
@@ -0,0 +1,18 @@
+from random import choice
+
+players = ["ahmed","borhan","chad","daymon","emon","falguni","giyas","hamid"]
+print(players)
+teamA = []
+teamB = []
+
+while len(players) > 0:
+    playerA = choice(players)
+    teamA.append(playerA)
+    players.remove(playerA)
+
+    playerB = choice(players)
+    teamB.append(playerB)
+    players.remove(playerB)
+
+print("Team A : ",teamA)
+print("Team B : ",teamB)
